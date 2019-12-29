a3530f3e08e58f7729c87d10f3de8d2005e60195
zakbarlow1995 <zak@stormideas.com>
2019-12-29 22:40:41 +0000

    Added a currency picker

--- Poker Chip Calculator/ViewControllers/ChipSetTableVC.swift
+++ Poker Chip Calculator/ViewControllers/ChipSetTableVC.swift
@@ -12,11 +12,45 @@ class ChipSetTableVC: UIViewController {
     
     let tableView = UITableView()
     
+    let pickerBlurView = UIView(backgroundColor: .black)
+    var containerView = UIView()
+    let picker = UIPickerView()
+    
+    lazy var currencySelectedButton: UIButton = {
+        let btn = UIButton(frame: CGRect(origin: .zero, size: desiredCurrencySelectedButtonSize))
+        btn.backgroundColor = Colors.blueGreen
+        btn.setTitle("Currency Selected", for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 21.0, weight: .bold)
+        btn.titleLabel?.adjustsFontSizeToFitWidth = true
+        btn.setTitleColor(.white, for: .normal)
+        btn.cornerRadius = desiredButtonSize.height/2.0
+        btn.addTarget(self, action: #selector(currencySelectedButtonPressed), for: .touchUpInside)
+        return btn
+    }()
+    
+    @objc func currencySelectedButtonPressed() {
+        print("currencySelectedButtonPressed")
+        
+        if let chipSetName = chipSetTextField.text, !chipSetName.isEmpty, picker.selectedRow(inComponent: 0) < pickerCurrencyData.count {
+            chipSets.append(ChipSet(name: chipSetName, currency: picker.selectedRow(inComponent: 0) == 0 ? nil : pickerCurrencyData[picker.selectedRow(inComponent: 0)] ))
+            
+            let indexPath = IndexPath(row: chipSets.count - 1, section: 0)
+            
+            tableView.beginUpdates()
+            tableView.insertRows(at: [indexPath], with: .automatic)
+            tableView.endUpdates()
+            chipSetTextField.text = nil
+        }
+        
+        [pickerBlurView, containerView, currencySelectedButton].forEach { $0.isHidden = true }
+    }
+    
     let titleLabel = UILabel(text: "Poker Chip Sets", font: UIFont.systemFont(ofSize: 34.0, weight: .bold), textColor: Colors.blueGreen, textAlignment: .left, numberOfLines: 1, sizeToFit: true, adjustsFontSizeToFitWidth: true)
     
     let chipSetTextField = CustomTextField(placeholder: "Enter Chip Set Name", font: UIFont.systemFont(ofSize: 19.0, weight: .regular), textAlignment: .left, textColor: .systemGray, tintColor: .systemGray, borderWidth: 1.0, borderColor: Colors.blueGreen, cornerRadius: 9.0)
     
     lazy var desiredButtonSize = CGSize(width: 120.0, height: 44.0)
+    lazy var desiredCurrencySelectedButtonSize = CGSize(width: 250.0, height: 44.0)
     
     lazy var addChipSetButton: UIButton = {
         let btn = UIButton(frame: CGRect(origin: .zero, size: desiredButtonSize))
@@ -31,24 +65,18 @@ class ChipSetTableVC: UIViewController {
     }()
         
     @objc func addChipSetButtonPressed() {
-        print("AccountEditVC - addChipSetButtonPressed")
-        
+        print("addChipSetButtonPressed")
+        view.endEditing(true)
         if let chipSetName = chipSetTextField.text, !chipSetName.isEmpty {
-            chipSets.append(ChipSet(name: chipSetName))
-            
-            let indexPath = IndexPath(row: chipSets.count - 1, section: 0)
-            
-            tableView.beginUpdates()
-            tableView.insertRows(at: [indexPath], with: .automatic)
-            tableView.endUpdates()
-            chipSetTextField.text = nil
-            view.endEditing(true)
+            [pickerBlurView, containerView, currencySelectedButton].forEach { $0.isHidden = false }
         }
     }
     
     private let chipSetIdentifier = "chipSetIdentifier"
     
     var chipSets: [ChipSet] = [ChipSet.monteCarloChipSet(currency: "$"), ChipSet.monteCarloChipSet(currency: "£"), ChipSet.random(), ChipSet.random(), ChipSet.random(), ChipSet.random(), ChipSet.random(), ChipSet.random()]
+    
+    private var pickerCurrencyData = ["None", "$", "¢", "£", "€", "¥", "֏", "৳", "৲", "৻", "૱", "௹", "฿", "៛", "₠", "₡", "₢", "₣", "₤", "₥", "₦", "₧", "₨", "₩", "₪", "₫", "₭", "₮", "₯", "gn", "₰", "ny", "₱", "₲", "₳", "₴", "₵", "₶", "₷", "₸", "₹", "₺", "₻", "₼", "₽", "₾", "₿", "රු", "Rs", "Fr", "ƒ", "NT$", "C$", "CHF", "Дин.", "ден", "kr", "zł", "L", "c", "n", "q", "فلس" ,"ملّيم" ,"ر.س"]
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -57,20 +85,9 @@ class ChipSetTableVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "+", style: .done, target: self, action: #selector(addChipSet))
-        
-//        navigationItem.title = "Poker Chip Sets"
-//        navigationController?.navigationBar.prefersLargeTitles = true
-        
         setupTopElements()
         setupTableView()
+        setupPickerView()
     }
     
     fileprivate func setupTopElements() {
@@ -92,17 +109,36 @@ class ChipSetTableVC: UIViewController {
         tableView.delegate = self
         tableView.register(ChipSetTableViewCell.self, forCellReuseIdentifier: chipSetIdentifier)
         tableView.keyboardDismissMode = .interactive
-//        tableView.bounces = false
         
         view.addSubview(tableView)
         tableView.anchor(top: addChipSetButton.bottomAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 8, left: 0, bottom: 0, right: 0))
         view.backgroundColor = Colors.crystalBlue
     }
     
-//    @objc fileprivate func addChipSet() {
-//        print("addChipSetButtonPressed")
-//    }
-    
+    fileprivate func setupPickerView() {
+        [pickerBlurView, containerView, currencySelectedButton].forEach { $0.isHidden = true }
+        
+        view.addSubviews(pickerBlurView, containerView, currencySelectedButton)
+        
+        pickerBlurView.fillSuperview()
+        pickerBlurView.alpha = 0.5
+        
+        containerView.centerInSuperview()
+        containerView.constrainWidth(desiredCurrencySelectedButtonSize.width)
+        containerView.constrainHeight(desiredCurrencySelectedButtonSize.width)
+        
+        containerView.cornerRadius = 9.0
+        
+        containerView.addSubview(picker)
+        
+        picker.delegate = self
+        picker.dataSource = self
+        
+        picker.backgroundColor = .systemGray
+        picker.fillSuperview()
+        
+        currencySelectedButton.anchor(top: containerView.bottomAnchor, leading: containerView.centerXAnchor, bottom: nil, trailing: nil, padding: .init(top: 16, left: -0.5*desiredCurrencySelectedButtonSize.width, bottom: 0, right: 0), size: desiredCurrencySelectedButtonSize)
+    }
 }
 
 extension ChipSetTableVC: UITableViewDataSource, UITableViewDelegate {
@@ -120,20 +156,19 @@ extension ChipSetTableVC: UITableViewDataSource, UITableViewDelegate {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: chipSetIdentifier, for: indexPath) as? ChipSetTableViewCell else { return UITableViewCell() }
 
         guard indexPath.row < chipSets.count else { return cell }
-//        cell.textLabel?.text = chipSets[indexPath.row].name
         
         cell.configure(with: chipSets[indexPath.row], delegate: self)
         
         return cell
     }
 
-    // Override to support conditional editing of the table view.
+    // To support conditional editing of the table view.
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
 
-    // Override to support editing the table view.
+    // To support editing the table view.
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
@@ -145,21 +180,6 @@ extension ChipSetTableVC: UITableViewDataSource, UITableViewDelegate {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 }
 
 extension ChipSetTableVC: ChipSetTableViewCellDelegate {
@@ -173,34 +193,28 @@ extension ChipSetTableVC: ChipSetTableViewCellDelegate {
 
 extension ChipSetTableVC: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        if let selectedTextFieldIndex = (textFields as [UITextField]).firstIndex(of: textField), selectedTextFieldIndex < textFields.count - 1 {
-//            textFields[selectedTextFieldIndex + 1].becomeFirstResponder()
-//        } else {
-            textField.resignFirstResponder()
-//        }
+        textField.resignFirstResponder()
         return true
     }
-    
-//    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
-//        print("textFieldDidEndEditing")
-//        if let viewModel = viewModel {
-//            viewModel.set(firstName: firstNameTextField.text, lastName: lastNameTextField.text, email: emailTextField.text, marketing: checkBox.isSelected)
-//            updateViewsAccordingToFieldValidation()
-//        }
-//        updateSignUpButton()
-//    }
-//
-//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-//        if let selectedTextFieldIndex = (textFields as [UITextField]).firstIndex(of: textField), selectedTextFieldIndex < textFieldsHaveBeenModified.count, !textFieldsHaveBeenModified[selectedTextFieldIndex] {
-//            switch selectedTextFieldIndex {
-//            case 0: hasFirstNameTextFieldBeenModified = true
-//            case 1: hasLastNameTextFieldBeenModified = true
-//            case 2: hasEmailTextFieldBeenModified = true
-//            default: break
-//            }
-//        }
-//        return true
-//    }
-    
 }
 
+extension ChipSetTableVC: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerCurrencyData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        guard row < pickerCurrencyData.count else { return nil }
+        return pickerCurrencyData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //Do something...
+        guard row < pickerCurrencyData.count else { return }
+        print(pickerCurrencyData[row])
+    }
+}
