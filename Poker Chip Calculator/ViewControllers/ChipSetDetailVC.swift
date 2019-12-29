a70b5dd6702f711655222f3862036ae7147b420c
zakbarlow1995 <zak@stormideas.com>
2019-12-29 23:23:54 +0000

    Can now add stacks of chips in ChipSetDEtailVC

--- Poker Chip Calculator/ViewControllers/ChipSetDetailVC.swift
+++ Poker Chip Calculator/ViewControllers/ChipSetDetailVC.swift
@@ -24,6 +24,50 @@ class ChipSetDetailVC: UIViewController {
     private let chipStackIdentifier = "chipStackIdentifier"
     var viewModel: ChipSetDetailViewModel?
     
+    let chipValueTextField = NumericTextField(placeholder: "Enter Chip Value", font: UIFont.systemFont(ofSize: 19.0, weight: .regular), textAlignment: .left, textColor: .systemGray, tintColor: .systemGray, borderWidth: 1.0, borderColor: Colors.blueGreen, cornerRadius: 9.0)
+    let chipNumberTextField = NumericTextField(placeholder: "Enter Number Of Chips", font: UIFont.systemFont(ofSize: 19.0, weight: .regular), textAlignment: .left, textColor: .systemGray, tintColor: .systemGray, borderWidth: 1.0, borderColor: Colors.blueGreen, cornerRadius: 9.0)
+    
+    lazy var desiredButtonSize = CGSize(width: 120.0, height: 44.0)
+    lazy var desiredCurrencySelectedButtonSize = CGSize(width: 250.0, height: 44.0)
+    
+    lazy var addChipStackButton: UIButton = {
+        let btn = UIButton(frame: CGRect(origin: .zero, size: desiredButtonSize))
+        btn.backgroundColor = Colors.blueGreen
+        btn.setTitle("Add", for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 21.0, weight: .bold)
+        btn.titleLabel?.adjustsFontSizeToFitWidth = true
+        btn.setTitleColor(.white, for: .normal)
+        btn.cornerRadius = desiredButtonSize.height/2.0
+        btn.addTarget(self, action: #selector(addChipSetButtonPressed), for: .touchUpInside)
+        return btn
+    }()
+        
+    @objc func addChipSetButtonPressed() {
+        print("addChipSetButtonPressed")
+        view.endEditing(true)
+        if let viewModel = self.viewModel, let chipValueText = chipValueTextField.text, let chipNumberText = chipNumberTextField.text, !chipValueText.isEmpty, !chipNumberText.isEmpty, let chipValue = Int(chipValueText), let chipNumber = Int(chipNumberText) {
+            
+            
+            if let stackToUpdate = viewModel.chipSet.stacks.first(where: { $0.value == chipValue }), let index = viewModel.chipSet.stacks.firstIndex(where: { $0.value == chipValue }) {
+                stackToUpdate.number += chipNumber
+                let indexPath = IndexPath(row: index, section: 0)
+                tableView.beginUpdates()
+                tableView.reloadRows(at: [indexPath], with: .automatic)
+                tableView.endUpdates()
+            } else {
+                viewModel.chipSet.stacks.append(ChipStack(value: chipValue, number: chipNumber, currency: viewModel.chipSet.currency, title: nil, color: .random))
+                
+                let indexPath = IndexPath(row: viewModel.chipSet.stacks.count - 1, section: 0)
+                
+                tableView.beginUpdates()
+                tableView.insertRows(at: [indexPath], with: .automatic)
+                tableView.endUpdates()
+            }
+            chipValueTextField.text = nil
+            chipNumberTextField.text = nil
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupTransparentNavigationBarWithWhiteText()
@@ -31,12 +75,6 @@ class ChipSetDetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "+", style: .done, target: self, action: #selector(addChipSet))
-        
-//        navigationItem.title = viewModel?.chipSet.name
-//        navigationController?.navigationBar.prefersLargeTitles = true
-        
         titleLabel.text = viewModel?.chipSet.name
         
         // Remove "Back" text from navigation bar, to be left with just " < " chevron
@@ -49,9 +87,18 @@ class ChipSetDetailVC: UIViewController {
     }
     
     fileprivate func setupTopElements() {
-        view.addSubview(titleLabel)
+        view.addSubviews(titleLabel, addChipStackButton, chipValueTextField, chipNumberTextField)
         
         titleLabel.anchor(top: view.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 24.0, left: 30, bottom: 0, right: 16))
+        
+        addChipStackButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 8, left: 0, bottom: 0, right: 16), size: desiredButtonSize)
+        
+        chipValueTextField.anchor(top: addChipStackButton.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: addChipStackButton.bottomAnchor, trailing: addChipStackButton.leadingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16))
+        
+        chipNumberTextField.anchor(top: chipValueTextField.bottomAnchor, leading: chipValueTextField.leadingAnchor, bottom: nil, trailing: chipValueTextField.trailingAnchor, padding: .init(top: 8, left: 0, bottom: 0, right: 0), size: .init(width: chipValueTextField.bounds.width, height: desiredButtonSize.height))
+        
+        chipValueTextField.delegate = self
+        chipNumberTextField.delegate = self
     }
         
     fileprivate func setupTableView() {
@@ -61,7 +108,7 @@ class ChipSetDetailVC: UIViewController {
         tableView.delegate = self
         tableView.register(ChipSetDetailCell.self, forCellReuseIdentifier: chipStackIdentifier)
         
-        tableView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 8, left: 0, bottom: 0, right: 0))
+        tableView.anchor(top: chipNumberTextField.bottomAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 8, left: 0, bottom: 0, right: 0))
         view.backgroundColor = Colors.crystalBlue
     }
     
@@ -94,11 +141,37 @@ extension ChipSetDetailVC: UITableViewDataSource, UITableViewDelegate {
         
         return cell
     }
+    
+    // To support conditional editing of the table view.
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
 
+    // To support editing the table view.
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard let viewModel = self.viewModel else { return }
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            viewModel.chipSet.stacks.remove(at: indexPath.row)
+            tableView.beginUpdates()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.endUpdates()
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
 }
 
 extension ChipSetDetailVC: ChipSetDetailCellDelegate {
     func chipStackCellPressed(chipStack: ChipStack) {
         print("DELEGATION - Chip stack button pressed - \(chipStack.formattedTotalValue)!")
+    }
+}
+
+extension ChipSetDetailVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
