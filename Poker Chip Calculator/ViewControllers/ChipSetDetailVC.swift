87b56f394751fc8df3ecb96557d65df08dd7dfaf
zakbarlow1995 <zak@stormideas.com>
2019-12-30 15:42:57 +0000

    Can now reorder stacks of chips into ascending or descending order

--- Poker Chip Calculator/ViewControllers/ChipSetDetailVC.swift
+++ Poker Chip Calculator/ViewControllers/ChipSetDetailVC.swift
@@ -56,32 +56,38 @@ class ChipSetDetailVC: UIViewController {
             [pickerBlurView, containerView, colorSelectedButton].forEach { $0.isHidden = false }
         }
     }
+    
+    lazy var reOrderChipStackButton: UIButton = {
+        let btn = UIButton(frame: CGRect(origin: .zero, size: desiredButtonSize))
+        btn.backgroundColor = Colors.blueGreen
+        btn.setTitle("Re-Order", for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 21.0, weight: .bold)
+        btn.titleLabel?.adjustsFontSizeToFitWidth = true
+        btn.setTitleColor(.white, for: .normal)
+        btn.cornerRadius = desiredButtonSize.height/2.0
+        btn.addTarget(self, action: #selector(reOrderChipStackButtonPressed), for: .touchUpInside)
+        return btn
+    }()
+    
+    @objc func reOrderChipStackButtonPressed() {
+        print("reOrderChipStackButtonPressed")
+        view.endEditing(true)
         
-//    @objc func addChipSetButtonPressed() {
-//        print("addChipSetButtonPressed")
-//        view.endEditing(true)
-//        if let viewModel = self.viewModel, let chipValueText = chipValueTextField.text, let chipNumberText = chipNumberTextField.text, !chipValueText.isEmpty, !chipNumberText.isEmpty, let chipValue = Int(chipValueText), let chipNumber = Int(chipNumberText) {
-//            
-//            
-//            if let stackToUpdate = viewModel.chipSet.stacks.first(where: { $0.value == chipValue }), let index = viewModel.chipSet.stacks.firstIndex(where: { $0.value == chipValue }) {
-//                stackToUpdate.number += chipNumber
-//                let indexPath = IndexPath(row: index, section: 0)
-//                tableView.beginUpdates()
-//                tableView.reloadRows(at: [indexPath], with: .automatic)
-//                tableView.endUpdates()
-//            } else {
-//                viewModel.chipSet.stacks.append(ChipStack(value: chipValue, number: chipNumber, currency: viewModel.chipSet.currency, title: nil, color: .random))
-//                
-//                let indexPath = IndexPath(row: viewModel.chipSet.stacks.count - 1, section: 0)
-//                
-//                tableView.beginUpdates()
-//                tableView.insertRows(at: [indexPath], with: .automatic)
-//                tableView.endUpdates()
-//            }
-//            chipValueTextField.text = nil
-//            chipNumberTextField.text = nil
-//        }
-//    }
+        guard let viewModel = self.viewModel else { return }
+        
+        
+        if viewModel.chipSet.stacks.map({$0.value}) == viewModel.chipSet.stacks.sorted(by: {$0.value < $1.value }).map({$0.value}) {
+            viewModel.chipSet.stacks = viewModel.chipSet.stacks.sorted(by: {$0.value > $1.value })
+        } else {
+            viewModel.chipSet.stacks = viewModel.chipSet.stacks.sorted(by: {$0.value < $1.value })
+        }
+        
+//        viewModel.chipSet.stacks = viewModel.chipSet.stacks.sorted(by: {$0.value < $1.value })
+        
+        tableView.beginUpdates()
+        tableView.reloadData()
+        tableView.endUpdates()
+    }
     
     let pickerBlurView = UIView(backgroundColor: .black)
     let containerView = UIView()
@@ -148,7 +154,7 @@ class ChipSetDetailVC: UIViewController {
     }
     
     fileprivate func setupTopElements() {
-        view.addSubviews(titleLabel, addChipStackButton, chipValueTextField, chipNumberTextField)
+        view.addSubviews(titleLabel, addChipStackButton, chipValueTextField, chipNumberTextField, reOrderChipStackButton)
         
         titleLabel.anchor(top: view.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 24.0, left: 30, bottom: 0, right: 16))
         
@@ -157,6 +163,8 @@ class ChipSetDetailVC: UIViewController {
         chipValueTextField.anchor(top: addChipStackButton.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: addChipStackButton.bottomAnchor, trailing: addChipStackButton.leadingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16))
         
         chipNumberTextField.anchor(top: chipValueTextField.bottomAnchor, leading: chipValueTextField.leadingAnchor, bottom: nil, trailing: chipValueTextField.trailingAnchor, padding: .init(top: 8, left: 0, bottom: 0, right: 0), size: .init(width: chipValueTextField.bounds.width, height: desiredButtonSize.height))
+        
+        reOrderChipStackButton.anchor(top: addChipStackButton.bottomAnchor, leading: addChipStackButton.leadingAnchor, bottom: nil, trailing: addChipStackButton.trailingAnchor, padding: .init(top: 8, left: 0, bottom: 0, right: 0), size: desiredButtonSize)
         
         chipValueTextField.delegate = self
         chipNumberTextField.delegate = self
@@ -192,7 +200,6 @@ class ChipSetDetailVC: UIViewController {
         
         containerView.addSubview(colorPickerView)
         colorPickerView.fillSuperview()
-        //colorPickerView.color = .random
         
         colorSelectedButton.anchor(top: containerView.bottomAnchor, leading: containerView.centerXAnchor, bottom: nil, trailing: nil, padding: .init(top: 16, left: -0.5*desiredColorSelectedButtonSize.width, bottom: 0, right: 0), size: desiredColorSelectedButtonSize)
     }
